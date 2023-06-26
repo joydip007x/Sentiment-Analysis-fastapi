1d1d62cdea2a5898c04f0e8573e36b7019835cd4
Joydip Das <joydip007x@gmail.com>
2023-06-26 19:15:38 +0600

    Update main.py

--- main.py
+++ main.py
@@ -25,5 +25,6 @@ async def validation_exception_handler(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main:app')
+    uvicorn.run('main:app', host='0.0.0.0', port=8000 )
+
 
